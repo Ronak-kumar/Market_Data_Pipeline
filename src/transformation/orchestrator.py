a81940ec8f98f1c779ce4c1abd4d8f3c4e005c40
Ronak <ronakkumar528@gmail.com>
2026-09-19 13:02:38 +0530

    Finalized the transformation code

--- src/transformation/orchestrator.py
+++ src/transformation/orchestrator.py
@@ -22,32 +22,3 @@ def transform_data(filemap: dict[str: Path]) -> None:
             saving_path = Path(*parts[:-1])
             saving_path.mkdir(parents=True, exist_ok=True)
             segment_frame.write_parquet(saving_path / f"{segment}.parquet")
-
-
-### Debugging purpose only ####
-if __name__ == "__main__":
-    transformeer = UpstoxTransformationAdapter()
-    filemap = {datetime.date(2026, 9, 18): 
-            {'NSE_INDEX': Path('D:/Development/Coding_Projects/Main_projects/Market_Data_Pipeline/src/extraction/cache/upstox/2026-09-18/NSE_INDEX.parquet'),
-            'NSE_FO': Path('D:/Development/Coding_Projects/Main_projects/Market_Data_Pipeline/src/extraction/cache/upstox/2026-09-18/NSE_FO.parquet'),
-            'BSE_INDEX': Path('D:/Development/Coding_Projects/Main_projects/Market_Data_Pipeline/src/extraction/cache/upstox/2026-09-18/BSE_INDEX.parquet'),
-            'BSE_FO': Path('D:/Development/Coding_Projects/Main_projects/Market_Data_Pipeline/src/extraction/cache/upstox/2026-09-18/BSE_FO.parquet'),
-            'MCX_FO': Path('D:/Development/Coding_Projects/Main_projects/Market_Data_Pipeline/src/extraction/cache/upstox/2026-09-18/MCX_FO.parquet')}}
-
-    for date, filepaths in filemap.items():
-        print(f"Date: {date}")
-        for segment, filepath in filepaths.items():
-            print(f"Segment: {segment}, Filepath: {filepath}")
-
-            normalized_df = transformeer.base_transformation(filepath=filepath, segment=segment)
-
-            if "MCX" in segment:
-                segment_frame = transformeer.mcx_transformation(normalized_df)
-            elif "INDEX" in segment:
-                segment_frame = transformeer.equity_transformation(normalized_df)
-            elif "FO" in segment:
-                segment_frame = transformeer.fno_transformation(normalized_df)
-
-            saving_path = filepath.replace("Bronze", "Silver")
-            saving_path.mkdir(parents=True, exist_ok=True)
-            segment_frame.write_parquet(saving_path / f"{segment}.parquet")
